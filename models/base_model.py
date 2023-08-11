548777f7a55de8c19d46b273e51e02321b21e603
kry01 <brahim.bousskry@edu.uiz.ac.ma>
2023-08-11 18:19:00 +0100

    [UPDATE] edit code

--- models/base_model.py
+++ models/base_model.py
@@ -12,18 +12,43 @@ class BaseModel:
     """
     class BaseModel that defines all common att/meth for other classes.
     """
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+    def __init__(self, *args, **kwargs):
+        """
+        the constructor of a BaseModel.
+        """
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key in ["created_at", "updated_at"]:
+                    f = "%Y-%m-%dT%H:%M:%S.%f"
+                    setattr(self, key, datetime.strptime(value, f))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
+        """
+        return the string format of a instance.
+        """
         return "[<BaseModel>] ({}) <{}>".format(self.id, self.__dict__)
 
     def save(self):
-        self.updated_at = datetime.datetime.now()
+        """
+        updates the updated_at with the current datetime
+        """
+
+        self.updated_at = datetime.now()
 
     def to_dict(self):
+        """
+        returns a dictionary containing all keys/values of __dict__
+        """
+
         dictionary = self.__dict__.copy()
         dictionary["__class__"] = self.__class__.__name__
         dictionary["created_at"] = str(datetime.isoformat(self.created_at))
