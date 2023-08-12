db39b3236151e8d159980ebe8bfd799df8dcd9df
Redouane AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-08-13 00:07:45 +0100

    Update File console.py

--- console.py
+++ console.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+""" Entry point of the command interpreter """
 import cmd
 from models import storage
 from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
-
+    """Command"""
+    
     prompt = "(hbnb) "
     
     def help_help(self):
