66d2c3707c89d52c681f3ca20688bbae680408bc
Redouane AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-08-13 00:15:11 +0100

    Update File console.py

--- console.py
+++ console.py
@@ -3,19 +3,21 @@
 import cmd
 from models import storage
 from models.base_model import BaseModel
-import json
+from models.user import User
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models.state import State
+from models.review import Review
 import shlex
 
 
 class HBNBCommand(cmd.Cmd):
     """Command"""
     
+    l_c = ['BaseModel', 'User', 'Amenity', 'Place', 'City', 'State', 'Review']
     prompt = "(hbnb) "
     
-    def help_help(self):
-        """ Prints help command description """
-        print("Provides description of a given command")
-
     def emptyline(self):
         """do nothing when empty line"""
         pass
