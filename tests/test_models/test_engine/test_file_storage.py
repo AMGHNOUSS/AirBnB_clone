ef518ccdf81fdd52cc61b8b01e9efac6b6885ffa
kry01 <brahim.bousskry@edu.uiz.ac.ma>
2023-08-13 00:13:35 +0100

    add test file for fileStorage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -6,6 +6,8 @@ import unittest
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models import storage
+import os
+import json
 
 
 class TestFileStorage(unittest.TestCase):
@@ -45,4 +47,46 @@ class TestFileStorage(unittest.TestCase):
         self.assertNotEqual(updated, dictonary["updated_at"])
         self.assertEqual(dictonary["name"], "bousskry")
 
-        def testHasAttributes(self):
+    def testHasAttributes(self):
+        """verify if attributes exist"""
+        self.assertEqual(hasattr(FileStorage, '_FileStorage__file_path'), True)
+        self.assertEqual(hasattr(FileStorage, '_FileStorage__objects'), True)
+
+    def testsave(self):
+        """verify if JSON exists"""
+        self.model.save()
+        self.assertEqual(os.path.exists(storage._FileStorage__file_path), True)
+        self.assertEqual(storage.all(), storage._FileStorage__objects)
+
+    def testreload(self):
+        """test if reload """
+        self.model.save()
+        self.assertEqual(os.path.exists(storage._FileStorage__file_path), True)
+        obj = storage.all()
+        FileStorage._FileStorage__objects = {}
+        self.assertNotEqual(obj, FileStorage._FileStorage__objects)
+        storage.reload()
+        for key, value in storage.all().items():
+            self.assertEqual(obj[key].to_dict(), value.to_dict())
+
+    def testSaveSelf(self):
+        """ Check save self """
+        msg = "FileStorage.save() takes 1 positional argument but 2 were given"
+        with self.assertRaises(TypeError) as e:
+            FileStorage.save(self, 100)
+
+        self.assertEqual(str(e.exception), msg)
+
+    def test_save_FileStorage(self):
+        """ Test if 'new' method is working good """
+        var1 = self.model.to_dict()
+        new_key = var1['__class__'] + "." + var1['id']
+        storage.save()
+        with open("file.json", 'r') as fd:
+            var2 = json.load(fd)
+        new = var2[new_key]
+        for key in new:
+            self.assertEqual(var1[key], new[key])
+
+if __name__ == '__main__':
+    unittest.main()
