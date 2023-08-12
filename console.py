620a2e64ea735a78b885cf08961b1cd7d3c272ca
Redouane AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-08-12 23:15:33 +0100

    Create File console.py

---
+++ console.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import cmd
+from models import storage
+from models.base_model import BaseModel
+
+class HBNBCommand(cmd.Cmd):
+
+    prompt = "(hbnb) "
+    
+    def help_help(self):
+        """Prints help command description """
+        print("Provides description of a given command")
+
+    def emptyline(self):
+        """do nothing when empty line"""
+        pass
+
+    def do_quit(self, line):
+        """Quit command to exit the program 
+        """
+        return True
+    
+    def do_EOF(self, line):
+        """EOF command to exit the command interpreter """
+        return True
+    
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
